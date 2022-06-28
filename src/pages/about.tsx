dbc1d38c8bdb717e6c010ce623ca1a5a957ce99b
coycoding <mattcoycoding@gmail.com>
2022-06-27 23:44:10 -0400

    fix return about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,9 +2,10 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 import { useRouter } from 'next/router';
 
-const About = () => {const router = useRouter();
+const About = () => {
+  const router = useRouter();
   console.log(router);
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  return (<Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
