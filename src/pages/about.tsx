cdcffd404369bcfc3a331886e2ea79726423cdb1
coycoding <mattcoycoding@gmail.com>
2022-06-27 23:49:17 -0400

    feat(pill): build fix

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,27 @@
+import { useRouter } from 'next/router';
+
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
-import { useRouter } from 'next/router';
 
 const About = () => {
   const router = useRouter();
   console.log(router);
-  return (<Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-  </Main>
-)};
+  return (
+    <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+        labore voluptatibus distinctio recusandae autem esse explicabo molestias
+        officia placeat, accusamus aut saepe.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+        labore voluptatibus distinctio recusandae autem esse explicabo molestias
+        officia placeat, accusamus aut saepe.
+      </p>
+    </Main>
+  );
+};
 
 export default About;
